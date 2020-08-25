6fa2a801cce5b11a4052f3ff05c2b647afa2dde4
doyougnu <jmy6342@gmail.com>
2020-08-25 10:21:50 -0700

    re-enabling the local server

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,22 +1,22 @@
 module Main where
 
--- import Web.Spock hiding (Var)
--- import Web.Spock.Config
--- import System.Environment (getEnv)
--- import System.IO.Error (catchIOError)
+import Web.Spock hiding (Var)
+import Web.Spock.Config
+import System.Environment (getEnv)
+import System.IO.Error (catchIOError)
+import Server
 
 -- Keeping unused imports for repl experience, see docs
 -- import VProp.Types
 -- import VProp.Gen
 -- import Data.Aeson (decode)
 -- import qualified Data.ByteString.Lazy.Char8 as B (putStrLn)
--- import Server
 
 -- pprint = B.putStrLn
 
 main :: IO ()
-main = return ()
-  -- do
-  -- port <- read <$> (getEnv "PORT") `catchIOError` const (return ("8080" :: String))
-  -- spockCfg <- defaultSpockCfg () PCNoDatabase ()
-  -- runSpock port (spock spockCfg app)
+main =
+  do
+  port <- read <$> (getEnv "PORT") `catchIOError` const (return ("8080" :: String))
+  spockCfg <- defaultSpockCfg () PCNoDatabase ()
+  runSpock port (spock spockCfg app)
